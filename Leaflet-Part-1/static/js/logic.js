b00793f29d668b910280c3af52561ffc2df80086
timsullivan92 <89842238+timsullivan92@users.noreply.github.com>
2024-11-05 14:36:07 -0500

    Final update

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,70 +1,112 @@
-// Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features);
-  });
-
-  function createFeatures(earthquakeData) {
-
-    // Define a function that we want to run once for each feature in the features array.
-    // Give each feature a popup that describes the place and time of the earthquake.
-    function onEachFeature(feature, layer) {
-      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-    }
-  
-    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-    // Run the onEachFeature function once for each piece of data in the array.
-    let earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-  
-    // Send our earthquakes layer to the createMap function/
-    createMap(earthquakes);
-  }
+// Overlay group
+var earthquakeLayer = new L.layerGroup();
 
 
-  function createMap(earthquakes) {
+var overlays = {
+    Earthquakes: earthquakeLayer
+}
 
-    // Create the base layers.
-    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-  
-    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+// Adding the tile layers
+var geoLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href=https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors'
+})
+
+/*var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    });
-  
-    // Create a baseMaps object.
-    let baseMaps = {
-      "Street Map": street,
-      "Topographic Map": topo
-    };
-  
-    // Create an overlay object to hold our overlay.
-    let overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    let myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [street, earthquakes]
-    });
-  
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  
-  }
+  });
+*/
+
+// base layers
+var baseLayers = {
+    Geo: geoLayer
+} 
+
+// Creating the map object
+var myMap = L.map("map", {
+    center: [37.6000, -95.6650],
+    zoom: 3.5, 
+    // Display on load
+    layers: [geoLayer]
+});
+
+// Layer control
+L.control.layers(baseLayers,overlays, {
+    collapsed: false
+  }).addTo(myMap);
+
+// Getting the colors for the circles and legend based on depth
+function mapColor(depth) {
+    return depth >= 90 ? "#FF0D0D" :
+        depth < 90 && depth >= 70 ? "#FF4E11" :
+        depth < 70 && depth >= 50 ? "#FF8E15" :
+        depth < 50 && depth >= 30 ? "#FFB92E" :
+        depth < 30 && depth >= 10 ? "#ACB334" :
+                                    "#69B34C";
+}
+
+// Drawing the circles
+function drawCircle(point, latlng) {
+    let mag = point.properties.mag;
+    let depth = point.geometry.coordinates[2];
+    return L.circle(latlng, {
+            fillOpacity: 0.5,
+            color: mapColor(depth),
+            fillColor: mapColor(depth),
+            // The size of the circle is based on magnitude of the earthquake
+            radius: mag * 20000
+    })
+}
+
+// Displaying info when the feature is clicked
+function bindPopUp(feature, layer) {
+    layer.bindPopup(`Location: ${feature.properties.place} <br> Magnitude: ${feature.properties.mag} <br> Depth: ${feature.geometry.coordinates[2]}`);
+}
+
+// The link to get the Earthquak GeoJSON data
+var url = " https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Getting the GeoJSON data
+d3.json(url).then((data) => {
+    var features = data.features;
+
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJSON(features, {
+        pointToLayer: drawCircle,
+        onEachFeature: bindPopUp
+    }).addTo(earthquakeLayer);
+
+    // Setting up the legend
 
 
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function() {
+        let div = L.DomUtil.create('div', 'info legend');
+        grades = [-10, 10, 30, 50, 70, 90];
+
+        // Looping through our intervals and generating a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + mapColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+        return div;
+    };
+
+legend.addTo(myMap)
+})
+
+
+// The link to get the tectonic plate boundaries data
+/*var tectonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+d3.json(tectonicURL).then((tectData) => {
+    L.geoJSON(tectData, {
+        color: "rgb(255, 94, 0)",
+        weight: 2
+    }).addTo(tectLayer);
+
+    tectLayer.addTo(myMap);
+})
+*/
   
